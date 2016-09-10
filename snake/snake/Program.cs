6246db9d3f5af8da8ae54d70a9578f9898f98141
Asim1431 <max.lizard@yandex.ru>
2016-09-10 11:48:36 +0300

    Создали класс Point

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,7 +10,21 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет!");
+            Point point1 = new snake.Point();
+            point1.x = 1;
+            point1.y = 3;
+            point1.symbol = '*';
+
+            point1.Draw();
+
+            Point point2 = new snake.Point();
+            point2.x = 5;
+            point2.y = 6;
+            point2.symbol = '#';
+
+            point2.Draw();
+
+
             Console.ReadLine();
         }
     }
